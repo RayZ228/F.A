1f729ca78e5782099df58ecf7b83856fcfcf9a29
RayZ228 <Almattildahmetov1@gmail.com>
2026-02-28 01:50:52 +0500

    Так же исправил импорты

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Text, BigInteger, DateTime, Date
 from sqlalchemy.sql import func
-from backend.database import Base
+from database import Base
 
 
 class Grant(Base):
